74304b9386f2a246493d165ee1b495efc3c55688
Janis <footbagfreestyle@gmail.com>
2013-12-30 00:29:40 +0000

    Fixed bug in fabfile.py where branch_name was not being passed to the command line

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 footbag-db
 ==========
 
-Footbag database
+This project aims create a set of educational tools accessible from the web
+to help people learn the skills and techniques involved in the sport of footbag.
+
+Note on git workflow:
+The assumption is that the master branch is always in such a state that the site
+can be deployed from that branch at any given time. Any commits to the master branch
+should pass all tests.
+
+Any features should be worked on in their own branch then deployed to the main site
+using the Fabric deployment.
+
+Note on installation:
+In this project I have used virtualenvwrapper to make sure that the versions of software used are consistent.
+
+If you wish to run this site on your own computer you will need to set up a virtualenv
+and install all the packages listed in requirements.txt.
--- footbag_site/fabfile.py
+++ footbag_site/fabfile.py
@@ -1,13 +1,25 @@
+""" This fabfile is designed to help deploy the site using git. The workflow being
+used here assumes that the master branch is for a the deployable site.
+"""
+
 from fabric.api import lcd, local
 
 def prepare_deployment(branch_name):
-    """ Prepare to deploy from a git branch if and only if the unit tests pass """
+    """ Prepare to deploy from a git branch if and only if the unit tests pass
+    The syntax to call this from the command line is:
+    fab prepare_deployment:branch_name
+    where branch_name is the name of the branch being deployed
+    """
     local('python manage.py test footbag_site')
     local('git add -p && git commit')
+    local('git checkout master && git merge ' + branch_name)
 
 def deploy():
-    """ Deploy from the dev folder to the live site using git"""
+    """ Deploy from the dev folder to the live site using git, assumes changes have
+    already been prepared with prepare_deployment.
+    PythonAnywhere is set up such that touching the WSGI file restarts the server
+    """
     with lcd('~/footbagsite/www_footbag_info/footbag_site'):
         local('git pull ~/footbagsite/dev/www_footbag_info/footbagsite')
         local('python manage.py migrate footbagmoves')
-        #local('command to restart webserver')
+        local('touch /var/www/www_footbag_info_wsgi.py')#restarts PythonAnywhere server
