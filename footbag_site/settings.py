52e69f3b012139c2eff6ea66cad77fdba907ac99
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-01-08 21:44:06 +0000

    Added a simple homepage app to serve the requests to the base URL

--- footbag_site/settings.py
+++ footbag_site/settings.py
@@ -48,7 +48,8 @@ THIRD_PARTY_APPS = (
 )
 
 LOCAL_APPS = (
-    'footbagmoves',
+    'apps.footbagmoves',
+    'apps.home',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- footbag_site/urls.py
+++ footbag_site/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from apps.home import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'footbag_site.views.home', name='home'),
+    url(r'^$', 'apps.home.views.index', name='basic_homepage'),#match the bare domain name
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
