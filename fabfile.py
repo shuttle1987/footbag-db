8d878d17298fb863d1b3af8f54b003414f29d3c8
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-03-18 02:27:16 +0000

    Made the fabfile for deployments more modular

--- fabfile.py
+++ fabfile.py
@@ -4,13 +4,17 @@ used here assumes that the master branch is for a the deployable site.
 
 from fabric.api import lcd, local
 
+def run_tests():
+    """ Run the unit test suite """
+    local('python manage.py test footbag_site')
+
 def prepare_deployment(branch_name):
     """ Prepare to deploy from a git branch if and only if the unit tests pass
     The syntax to call this from the command line is:
     fab prepare_deployment:branch_name
     where branch_name is the name of the branch being deployed
     """
-    local('python manage.py test footbag_site')
+    run_tests()
     local('git add -p && git commit')
     local('git checkout master && git merge ' + branch_name)
 
@@ -22,4 +26,8 @@ def deploy():
     with lcd('~/footbagsite/www_footbag_info/'):
         local('git pull ~/footbagsite/dev-site/')
         local('python manage.py migrate footbagmoves')
-        local('touch /var/www/www_footbag_info_wsgi.py')#restarts PythonAnywhere server
+        restart_server()
+
+def restart_server():
+    """ The command to restart the web server """
+    local('touch /var/www/www_footbag_info_wsgi.py')#restarts PythonAnywhere server
