2938224d179009eb845ce83c5c4c4d38d0f678b6
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-07-12 17:03:40 +0000

    Created some stubs for footbag component and move models

--- apps/footbagmoves/models.py
+++ apps/footbagmoves/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# a model for a footbag component
+class Component(models.Model):
+    """ A model for a footbag move component
+    see https://github.com/shuttle1987/footbag-db/wiki/Abstract-model-for-footbag-moves
+    and https://github.com/shuttle1987/footbag-db/wiki/Data-structure-implementation-for-footbag-moves
+    """
+    name = model.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
+
+# a model for an entire footbag move
+class Component(models.Model):
+    """ A model for an entire footbag move 
+    see https://github.com/shuttle1987/footbag-db/wiki/Abstract-model-for-footbag-moves
+    and https://github.com/shuttle1987/footbag-db/wiki/Data-structure-implementation-for-footbag-moves
+    """
+    name = model.CharField(max_length=30)
+    def __unicode__(self):
+        return self.name
+
