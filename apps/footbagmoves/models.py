d82fcc95681c5e91cc9583ffaedf92a3f3ea30ce
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-07-12 17:11:19 +0000

    fixed typo and incorrect class name

--- apps/footbagmoves/models.py
+++ apps/footbagmoves/models.py
@@ -6,17 +6,17 @@ class Component(models.Model):
     see https://github.com/shuttle1987/footbag-db/wiki/Abstract-model-for-footbag-moves
     and https://github.com/shuttle1987/footbag-db/wiki/Data-structure-implementation-for-footbag-moves
     """
-    name = model.CharField(max_length=20)
+    name = models.CharField(max_length=20)
     def __unicode__(self):
         return self.name
 
 # a model for an entire footbag move
-class Component(models.Model):
+class Move(models.Model):
     """ A model for an entire footbag move 
     see https://github.com/shuttle1987/footbag-db/wiki/Abstract-model-for-footbag-moves
     and https://github.com/shuttle1987/footbag-db/wiki/Data-structure-implementation-for-footbag-moves
     """
-    name = model.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     def __unicode__(self):
         return self.name
 
