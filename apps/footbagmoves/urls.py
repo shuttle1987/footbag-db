d8016da4149f192bd6de3146fcbba5ecb3aa4944
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-07-13 05:08:04 +0000

    Added in some skeleton functionality for move components

--- apps/footbagmoves/urls.py
+++ apps/footbagmoves/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from apps.footbagmoves import views
 
 urlpatterns = patterns('', 
-    url(r'^$', views.component_index, name='component_index')
+    url(r'^$', views.component_index, name='component_index'),
+    url(r'^(?P<component_name>[\w-]+)/$', views.component_detail, name='component_detail'),
 )
