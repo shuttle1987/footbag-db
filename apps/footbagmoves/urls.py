9de6546bc5eace2407aaf469b67bae76b73a8797
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-07-27 03:41:17 +0000

    Changed name of captured argument in urls

--- apps/footbagmoves/urls.py
+++ apps/footbagmoves/urls.py
@@ -5,5 +5,5 @@ from apps.footbagmoves import views
 
 urlpatterns = patterns('', 
     url(r'^$', views.component_index, name='component_index'),
-    url(r'^(?P<component_name>[\w-]+)/$', views.component_detail, name='component_detail'),
+    url(r'^(?P<component_slug>[\w-]+)/$', views.component_detail, name='component_detail'),
 )
