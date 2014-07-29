cc3c6d5f2aea7c0730ead93e1cf3756ff3789faa
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-07-29 04:17:20 +0000

    Created better admin page handling for footbag move creation

--- apps/footbagmoves/admin.py
+++ apps/footbagmoves/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from apps.footbagmoves.models import Component, Move
+from apps.footbagmoves.models import Component, Move, MoveComponentSequence
+
+class MoveComponentSequenceInline(admin.TabularInline):
+    """ Inline admin for the move sequences. This is so we can edit the components
+    sequence in the same place in the Admin page as the moves"""
+    model = MoveComponentSequence
+
+class MoveAdmin(admin.ModelAdmin): 
+    """Admin helper for the moves """
+    inlines = [
+            MoveComponentSequenceInline,
+    ]
 
 admin.site.register(Component)
-admin.site.register(Move)
+admin.site.register(Move, MoveAdmin)
