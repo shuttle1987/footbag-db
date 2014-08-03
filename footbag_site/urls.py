639aaa0113ad616b4038a891d14a35fed01db689
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-08-03 23:07:14 +0000

    Created an about page, see issue #23

--- footbag_site/urls.py
+++ footbag_site/urls.py
@@ -10,6 +10,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'apps.home.views.index', name='basic_homepage'),#match the bare domain name
+    #url(r'^about.html', 'apps.home.views.about', name='about_page'),#match the about page
+    url(r'^about$', TemplateView.as_view(template_name='home/about.html'), name='about'),
     #url(r'^components/', include('apps.footbagmoves.urls', namespace='footbagmoves')),#namespace currently causing issues with url reverse matching in components index
     url(r'^components/', include('apps.footbagmoves.urls')),
     url(r'^moves/', include('apps.footbagmoves.move_urls')),
