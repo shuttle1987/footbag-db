395d03623e59dd3cf95153df28591849035f2533
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-08-16 02:40:22 +0000

    Wrote unit tests for parsing the ID from a youtube URL

--- apps/footbagmoves/tests.py
+++ apps/footbagmoves/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.footbagmoves.video_api_helpers import extract_yt_id
+
+class YoutubeIDExtraction(TestCase):
+    """Test functionality that extracts the youtube ID from a youtube URL"""
+
+    def test_extract_youtube_id(self):
+        """Tests that the ID gets extracted from a full youtube URL correctly """
+        self.assertEqual(extract_yt_id("http://www.youtube.com/watch?v=L-ZBwq9wW_s"),"L-ZBwq9wW_s")
+        self.assertEqual(extract_yt_id("https://www.youtube.com/watch?v=L-ZBwq9wW_s"),"L-ZBwq9wW_s")
+        self.assertEqual(extract_yt_id("http://youtube.com/watch?v=L-ZBwq9wW_s"),"L-ZBwq9wW_s")
+        self.assertEqual(extract_yt_id("https://youtube.com/watch?v=L-ZBwq9wW_s"),"L-ZBwq9wW_s")
+        self.assertEqual(extract_yt_id("http://youtu.be/kiBZbcvGrZI"),"kiBZbcvGrZI")
