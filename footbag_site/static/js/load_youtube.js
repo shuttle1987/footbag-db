5ed7f2636e5bd54f4aced2bd0bc19edd11c69afc
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-08-17 21:37:06 +0000

    Removed the onPlayerStateChange function as it's currently not used

--- footbag_site/static/js/load_youtube.js
+++ footbag_site/static/js/load_youtube.js
@@ -5,25 +5,12 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
+//This function calls the youtube_ready function after the youtube API code downloads.
 function onYouTubeIframeAPIReady() {
     youtube_ready();
 }
 
-// 4. The API will call this function when the video player is ready.
+//The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-
-function stopVideo() {
-    player.stopVideo();
 }
