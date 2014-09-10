47dfd0402ea3800018bff189b6ae9005c5291c85
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-09-10 03:50:51 +0000

    Added unit test for entering in video with invalid start and end times

--- apps/footbagmoves/tests.py
+++ apps/footbagmoves/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 from apps.footbagmoves.video_api_helpers import extract_yt_id
+from apps.footbagmoves.models import YOUTUBE_VIDEO_TYPE
+from apps.footbagmoves.forms import VideoEntryForm
 
 class YoutubeIDExtraction(TestCase):
     """Test functionality that extracts the youtube ID from a youtube URL"""
@@ -12,3 +14,18 @@ class YoutubeIDExtraction(TestCase):
         self.assertEqual(extract_yt_id("http://youtube.com/watch?v=L-ZBwq9wW_s"),"L-ZBwq9wW_s")
         self.assertEqual(extract_yt_id("https://youtube.com/watch?v=L-ZBwq9wW_s"),"L-ZBwq9wW_s")
         self.assertEqual(extract_yt_id("http://youtu.be/kiBZbcvGrZI"),"kiBZbcvGrZI")
+
+class VideoEntryFormTests(TestCase):
+    """Test video entry from forms validation"""
+    def test_start_end_times(self):
+        """Test that a start time occuring after an end time fails validation"""
+        form_data = {
+                'video_type': YOUTUBE_VIDEO_TYPE,
+                'URL': 'http://www.youtube.com/watch?v=DJ_uZiueQKg',
+                'use_end': True,
+                'use_start': True,
+                'start_time': 15,
+                'end_time': 5,
+        }
+        form = VideoEntryForm(data=form_data)
+        self.assertFalse(form.is_valid())
