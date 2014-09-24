7205c4ca2309090b3fab43a8492b0eea92f2ad54
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-09-24 01:29:02 +0000

    Created a logout option in the sidebar for logged in users

--- footbag_site/views.py
+++ footbag_site/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def error404(request):
@@ -29,6 +29,13 @@ def user_login(request):
     else:
         login_template = loader.get_template('login.html')
         return HttpResponse(login_template.render(context))
+
+
+@login_required
+def user_logout(request):
+    """Log a user out"""
+    logout(request)
+    return HttpResponseRedirect('/')
 
 @login_required
 def user_panel(request):
