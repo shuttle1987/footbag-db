a0bfb8a3fabdb895956d48477d3a25b2d092091e
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-10-09 04:10:33 +0000

    Create a browse page for the footbag components

--- apps/footbagmoves/browse_urls.py
+++ apps/footbagmoves/browse_urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 from apps.footbagmoves import browse_views
 
 urlpatterns = patterns('', 
+    url(r'^components/$', browse_views.ComponentList.as_view(), name='component-list'),
     url(r'^moves/$', browse_views.MoveList.as_view(), name='move-list'),
 )
--- apps/footbagmoves/browse_views.py
+++ apps/footbagmoves/browse_views.py
@@ -1,5 +1,11 @@
 from django.views.generic import ListView
-from apps.footbagmoves.models import Move
+from apps.footbagmoves.models import Component, Move
+
+class ComponentList(ListView):
+    """A list view for footbag components"""
+    model = Component
+    context_object_name = 'component_list'
+    paginate_by = 15
 
 class MoveList(ListView):
     """A list view for footbag moves"""
