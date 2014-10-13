8ea9fe8932beb6948566ce9191d0a1845b15c8e6
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-10-13 19:01:06 +0000

    Exposed component nicknames to component_detail template

--- apps/footbagmoves/views.py
+++ apps/footbagmoves/views.py
@@ -8,7 +8,7 @@ import itertools
 from apps.footbagmoves.models import Component, Move, MoveComponentSequence
 from apps.footbagmoves.models import ComponentTutorialVideo, ComponentDemonstrationVideo
 from apps.footbagmoves.models import MoveTutorialVideo, MoveDemonstrationVideo
-from apps.footbagmoves.models import MoveNickname
+from apps.footbagmoves.models import MoveNickname, ComponentNickname
 
 from apps.footbagmoves.models import YOUTUBE_VIDEO_TYPE, URL_VIDEO_TYPE
 from apps.footbagmoves.video_api_helpers import extract_first_yt_url, extract_yt_id
@@ -91,17 +91,19 @@ def component_detail(request, component_slug):
     If the component is found in the database the component_detail template is loaded
     and if not the component_not_found template is loaded."""
     try:
-        component = Component.objects.get(slug=component_slug)
+        current_component = Component.objects.get(slug=component_slug)
     except Component.DoesNotExist:
         template = loader.get_template('footbagmoves/component_not_found.html')
         context = RequestContext(request, {'component_slug' : component_slug})
         return HttpResponseNotFound(template.render(context))
     template = loader.get_template('footbagmoves/component_detail.html')
     #load component info from DB
-    demo_video = ComponentDemonstrationVideo.objects.filter(component__exact=component)
-    tutorial_video = ComponentTutorialVideo.objects.filter(component__exact=component)
+    demo_video = ComponentDemonstrationVideo.objects.filter(component__exact=current_component)
+    tutorial_video = ComponentTutorialVideo.objects.filter(component__exact=current_component)
+    nicknames = ComponentNickname.objects.filter(move__exact=current_component)
     context = RequestContext(request, {
-        'component' : component,
+        'component' : current_component,
+        'nicknames': nicknames,
         'video_demo': demo_video,
         'video_tutorial': tutorial_video,
     })
