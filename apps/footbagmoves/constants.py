fa4007f69c51c83f49e45eec7fafd697648d132a
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-03 23:21:29 +0000

    Changed strings for python3

--- apps/footbagmoves/constants.py
+++ apps/footbagmoves/constants.py
@@ -1,11 +1,11 @@
 """Constants for the footbag moves app are stored here"""
 
 #Raw URL pointing to a video
-URL_VIDEO_TYPE = u'1'
+URL_VIDEO_TYPE = '1'
 #URL pointing to a youtube video
-YOUTUBE_VIDEO_TYPE = u'2'
+YOUTUBE_VIDEO_TYPE = '2'
 #Possible choices for video types
 VIDEO_TYPES = (
-        (URL_VIDEO_TYPE, u'URL'),
-        (YOUTUBE_VIDEO_TYPE, u'Youtube'),
+        (URL_VIDEO_TYPE, 'URL'),
+        (YOUTUBE_VIDEO_TYPE, 'Youtube'),
 )
