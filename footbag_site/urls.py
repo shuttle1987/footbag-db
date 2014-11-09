38cb6f0fa0a51053388c80f584e1722c2462d912
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-09 17:25:03 -0500

    Started creating a skeleton structure for signing up new user accounts

--- footbag_site/urls.py
+++ footbag_site/urls.py
@@ -4,8 +4,10 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 from apps.home import views
 from apps.footbagmoves import views
+import user_accounts
 from .views import user_login, user_logout, user_panel
 
 urlpatterns = patterns('',
@@ -19,6 +21,7 @@ urlpatterns = patterns('',
     url(r'^logout/$', user_logout, name='logout'),
     url(r'^user_panel/$', user_panel, name='user_panel'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='home/robots.txt', content_type='text/plain'), name='robots'),
+    url(r"^account/signup/$", user_accounts.views.SignupView.as_view(), name="account_signup"),
     url(r'^account/', include('account.urls')),#django-user-accounts
     url(r'^admin/', include(admin.site.urls)),
 )
--- user_accounts/forms.py
+++ user_accounts/forms.py
@@ -1,2 +1,7 @@
 """User account management related forms"""
 from django import forms
+import account.forms
+
+class SignupForm(account.forms.SignupForm):
+    """Form for user account sign-up"""
+    name = forms.CharField(max_length=60) 
