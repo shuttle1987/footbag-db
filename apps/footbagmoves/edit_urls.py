34ef33ff364322ddd393f8a24b387f38ee62431b
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-13 19:03:38 -0500

    Added url routing for editing footbag components

---
+++ apps/footbagmoves/edit_urls.py
@@ -0,0 +1,10 @@
+"""URL processing for editing footbag moves and components"""
+from django.conf.urls import patterns, url
+
+from apps.footbagmoves import views
+
+urlpatterns = patterns('', 
+    #url(r'^$', views.edit_index, name='edit_index'),
+    url(r'^component/(?P<component_id>[0-9]+)/$', views.component_edit, name='component_edit'),
+    #url(r'^move/(?P<move_id>[0-9]+)/$', views.move_edit, name='move_edit'),
+)
