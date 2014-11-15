2e94b57a7c3fa3625dafa7dad455e773c2541470
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-15 18:23:54 -0500

    Cleaned up the component editing view and added check for component existing before rending the form

--- apps/footbagmoves/edit_views.py
+++ apps/footbagmoves/edit_views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext, loader
 
@@ -7,21 +8,25 @@ from django.forms.formsets import formset_factory
 from apps.footbagmoves.models import Component
 from apps.footbagmoves.forms import ComponentEditForm, VideoEntryForm, VideosFormset
 
+VideoEntryFormset = formset_factory(Component, VideoEntryForm, extra=1)
+
 @login_required
 def component_edit(request, component_id=None):
     """Edit a component or add a new one"""
-    VideoEntryFormset = formset_factory(Component, VideoEntryForm)
     if component_id is None:
         new_component = Component()
-        edit_form = ComponentEditForm()
-        demo_vids = VideoEntryFormset(instance=new_component)
+        edit_form = ComponentEditForm(request.POST or None)
+        demo_vids = VideoEntryFormset(request.POST or None, instance=new_component)
     else:
-        current_component = Component.objects.get(pk=component_id)
-        demo_vids = VideoEntryFormset(instance=current_component)
+        current_component = get_object_or_404(Component, pk=component_id)
+        demo_vids = VideoEntryFormset(request.POST or None, instance=current_component)
+        if current_component and demo_vids.is_valid:
+            print("Valid component and Valid formset!!")
         data = {
             'name': current_component.name,
         }
         edit_form = ComponentEditForm(data)
+
     context = RequestContext(request, {
         'edit_form': edit_form,
         'demo_vids': demo_vids,
