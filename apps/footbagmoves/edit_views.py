c035c300d19650cb457433aaa2c5dcf5cc9b86d4
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-18 22:43:19 -0500

    Created some code to save a new component to the database

--- apps/footbagmoves/edit_views.py
+++ apps/footbagmoves/edit_views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 
 from django.forms.models import inlineformset_factory
 #from django.forms.formsets import formset_factory
@@ -9,10 +10,7 @@ from django.forms.models import inlineformset_factory
 from apps.footbagmoves.models import Component, ComponentDemonstrationVideo
 from apps.footbagmoves.forms import ComponentEditForm, VideoEntryForm, VideosFormset
 
-#from apps.footbagmoves.video_assets_models import VideoAsset
 VideoEntryFormset = inlineformset_factory(Component, ComponentDemonstrationVideo, form=VideoEntryForm, formset=VideosFormset, extra=1, max_num=20)
-
-#VideoEntryFormset = formset_factory(Component, VideoEntryForm, extra=1, max_num=20)
 
 @login_required
 def component_edit(request, component_id=None):
@@ -21,10 +19,15 @@ def component_edit(request, component_id=None):
         new_component = Component()
         edit_form = ComponentEditForm(request.POST or None)
         demo_vids = VideoEntryFormset(request.POST or None, instance=new_component)
+        if demo_vids.is_valid() and edit_form.is_valid():
+            #todo check if component name is already taken
+            new_component.save()
+            demo_vids.save()
+            return HttpResponseRedirect(reverse('component_detail', args=[new_component.slug]))
     else:
         current_component = get_object_or_404(Component, pk=component_id)
         demo_vids = VideoEntryFormset(request.POST or None, instance=current_component)
-        if current_component and demo_vids.is_valid:
+        if current_component and demo_vids.is_valid():
             print("Valid component and Valid formset!!")
         data = {
             'name': current_component.name,
