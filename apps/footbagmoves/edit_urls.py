38099c34e02a608d3ce53ab80b41a9888d35eee9
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-30 14:31:41 -0500

    Improved url routing and added a link for logged in users to edit components on the component page

--- apps/footbagmoves/edit_urls.py
+++ apps/footbagmoves/edit_urls.py
@@ -5,7 +5,7 @@ from apps.footbagmoves import edit_views
 
 urlpatterns = patterns('', 
     #url(r'^$', views.edit_index, name='edit_index'),
-    url(r'^component/$', edit_views.component_edit, name='component_edit'),
-    url(r'^component/(?P<component_id>[0-9]+)/$', edit_views.component_edit, name='component_edit'),
+    url(r'^component/$', edit_views.component_edit, name='component-new'),
+    url(r'^component/(?P<component_id>\d+)/$', edit_views.component_edit, name='component-edit'),
     #url(r'^move/(?P<move_id>[0-9]+)/$', views.move_edit, name='move_edit'),
 )
