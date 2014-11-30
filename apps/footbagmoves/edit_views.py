e0af449d5d8c2821e399a8ee53fc52bc6fbe814e
Janis Lesinskis <footbagfreestyle@gmail.com>
2014-11-30 17:21:04 -0500

    Refactored the component modification views and created a new page for modifying existing entries in the database

--- apps/footbagmoves/edit_views.py
+++ apps/footbagmoves/edit_views.py
@@ -16,55 +16,77 @@ VideoEntryFormset = inlineformset_factory(Component, ComponentDemonstrationVideo
 def component_edit(request, component_id=None):
     """Edit a component or add a new one"""
     if component_id is None:
-        new_component = Component()
-        edit_form = ComponentEditForm(request.POST or None)
-        tips_form = TipsForm(request.POST or None)
-        demo_vids = VideoEntryFormset(request.POST or None, instance=new_component)
-        if demo_vids.is_valid() and edit_form.is_valid():
-            new_component.name = edit_form.cleaned_data.get("name")
-            existing_components = Component.objects.filter(slug=slugify(new_component.name))
-            if existing_components:
-                return HttpResponse("Error saving: component with slug {0} already exists!".format(slugify(new_component.name)))
-            else:
-                new_component.save()
-                demo_vids.save()
-                if tips_form.is_valid():
-                    ComponentTips.objects.create(
-                        component=new_component,
-                        tips=tips_form.cleaned_data.get("tips"),
-                        tips_markup_type='markdown',
-                    )
-                return HttpResponseRedirect(reverse('component_detail', args=[new_component.slug]))
+        return component_new(request)
     else:
-        current_component = get_object_or_404(Component, pk=component_id)
-        demo_vids = VideoEntryFormset(request.POST or None, instance=current_component)
-        try:
-            #load tips if possible
-            existing_tips = ComponentTips.objects.get(component=current_component)
-        except ComponentTips.DoesNotExist:
-            existing_tips = None
+        return component_modify(request, component_id)
+
+@login_required
+def component_modify(request, component_id):
+    """Modify an existing component in the database"""
+    current_component = get_object_or_404(Component, pk=component_id)
+    demo_vids = VideoEntryFormset(request.POST or None, instance=current_component)
+    try:
+        #load tips if possible
+        existing_tips = ComponentTips.objects.get(component=current_component)
+    except ComponentTips.DoesNotExist:
+        existing_tips = None
+    if existing_tips:
+        raw_text = existing_tips.tips.raw
+        tips_form = TipsForm(request.POST or {'tips': raw_text})
+    else:
+        tips_form = TipsForm(request.POST or None)
+    data = {
+        'name': current_component.name,
+    }
+    edit_form = ComponentEditForm(data)
+    if demo_vids.is_valid() and edit_form.is_valid() and tips_form.is_valid():
+        demo_vids.save()
         if existing_tips:
-            raw_text = existing_tips.tips.raw
-            tips_form = TipsForm(request.POST or {'tips': raw_text})
+            print("existing tips found for {0}".format(current_component.name))
+            print("Existing tips", existing_tips)
+            existing_tips.tips.raw = tips_form.cleaned_data.get("tips")
+            #existing_tips.tips= tips_form.cleaned_data.get("tips")
+            existing_tips.save()
         else:
-            tips_form = TipsForm(request.POST or None)
-        data = {
-            'name': current_component.name,
-        }
-        edit_form = ComponentEditForm(data)
-        if demo_vids.is_valid() and edit_form.is_valid() and tips_form.is_valid():
+            ComponentTips.objects.create(
+                component=current_component,
+                tips=tips_form.cleaned_data.get("tips"),
+                tips_markup_type='markdown',
+            )
+        return HttpResponseRedirect(reverse('component_detail', args=[current_component.slug]))
+
+    context = RequestContext(request, {
+        'component_name': current_component.name,
+        'edit_form': edit_form,
+        'tips_form': tips_form,
+        'demo_vids': demo_vids,
+    })
+    template = loader.get_template('footbagmoves/component_modify.html')
+    return HttpResponse(template.render(context))
+
+
+@login_required
+def component_new(request):
+    """Add a new component to the database"""
+    new_component = Component()
+    edit_form = ComponentEditForm(request.POST or None)
+    tips_form = TipsForm(request.POST or None)
+    demo_vids = VideoEntryFormset(request.POST or None, instance=new_component)
+    if demo_vids.is_valid() and edit_form.is_valid():
+        new_component.name = edit_form.cleaned_data.get("name")
+        existing_components = Component.objects.filter(slug=slugify(new_component.name))
+        if existing_components:
+            return HttpResponse("Error saving: component with slug {0} already exists!".format(slugify(new_component.name)))
+        else:
+            new_component.save()
             demo_vids.save()
-            if existing_tips:
-                print("existing tips found for {0}".format(current_component.name))
-                existing_tips.tips.raw_text = tips_form.cleaned_data.get("tips")
-                existing_tips.save()
-            else:
+            if tips_form.is_valid():
                 ComponentTips.objects.create(
-                    component=current_component,
+                    component=new_component,
                     tips=tips_form.cleaned_data.get("tips"),
                     tips_markup_type='markdown',
                 )
-            return HttpResponseRedirect(reverse('component_detail', args=[current_component.slug]))
+            return HttpResponseRedirect(reverse('component_detail', args=[new_component.slug]))
 
     context = RequestContext(request, {
         'edit_form': edit_form,
