6877454d1bff4a68fd32f732ad57752d93395f90
Janis Lesinskis <footbagfreestyle@gmail.com>
2015-06-13 18:25:47 -0400

    Fixed problem with paths

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from invoke import task, run
 def compile_scss():
     """Compile the SCSS and copy to relevant static directory"""
     run("pyscss ./scss/*.scss > ./static/basic_theme/css/style.css")
-    run("cp /static/basic_theme/css/style.css /footbag_site/static/basic_theme/css/style.css")
+    run("cp ./static/basic_theme/css/style.css ./footbag_site/static/basic_theme/css/style.css")
 
 @task
 def run_tests():
