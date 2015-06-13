69bc746b8a11cfe9ab212816ec610b000f851fe5
Janis Lesinskis <footbagfreestyle@gmail.com>
2015-06-13 14:57:15 -0400

    Created a task for compiling the SCSS files

---
+++ tasks.py
@@ -0,0 +1,7 @@
+from invoke import task, run
+
+@task
+def compile_scss():
+    """Compile the SCSS and copy to relevant static directory"""
+    run("pyscss ./scss/*.scss > ./static/basic_theme/css/style.css")
+    run("cp /static/basic_theme/css/style.css /footbag_site/static/basic_theme/css/style.css")
