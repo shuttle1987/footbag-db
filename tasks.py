16c57e10e39a186d2c1b5fabf615a51b4cd0f138
Janis Lesinskis <footbagfreestyle@gmail.com>
2015-06-13 17:33:05 -0400

    Created invoke command for running unit tests

--- tasks.py
+++ tasks.py
@@ -5,3 +5,8 @@ def compile_scss():
     """Compile the SCSS and copy to relevant static directory"""
     run("pyscss ./scss/*.scss > ./static/basic_theme/css/style.css")
     run("cp /static/basic_theme/css/style.css /footbag_site/static/basic_theme/css/style.css")
+
+@task
+def run_tests():
+    """Run the unit testing suite"""
+    run("python manage.py test")
