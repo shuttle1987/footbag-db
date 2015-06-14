aa6ad7c53abddfa10257af15e3e4e9ef2070453b
Janis Lesinskis <footbagfreestyle@gmail.com>
2015-06-13 20:50:12 -0400

    Added invoke command for restarting the web server

--- tasks.py
+++ tasks.py
@@ -10,3 +10,9 @@ def compile_scss():
 def run_tests():
     """Run the unit testing suite"""
     run("python manage.py test")
+
+def restart_server():
+    """The command to restart the web server.
+    PythonAnywhere is set up such that touching the WSGI file restarts the server.
+    Change this command to whatever the web server requires."""
+    run('touch /var/www/www_footbag_info_wsgi.py')#restarts PythonAnywhere server
