1ba05b2a77038686b7d08a36f7189cfa1737bef0
Janis <janis@jaggedverge.com>
2016-03-06 17:50:57 +1100

    Changed developer page to use template

--- apps/footbagmoves/developer_views.py
+++ apps/footbagmoves/developer_views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Developers page coming soon")
+    template = loader.get_template('footbagmoves/developer.html')
+    html = template.render({}, request)
+    return HttpResponse(html)
