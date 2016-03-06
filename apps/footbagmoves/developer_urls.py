7d13d352a30e0d1606031638f82e1f000a11d51a
Janis <janis@jaggedverge.com>
2016-03-07 00:32:58 +1100

    Changed json urls to .json

--- apps/footbagmoves/developer_urls.py
+++ apps/footbagmoves/developer_urls.py
@@ -6,6 +6,6 @@ from apps.footbagmoves import developer_views
 urlpatterns = [
     url(r'^$', developer_views.index, name='developer-index'),
     url(r'^index$', developer_views.index, name='developer-index'),
-    url(r'^components_json$', developer_views.jsonComponentsView, name='components-json'),
-    url(r'^moves_json$', developer_views.jsonMovesView, name='moves-json'),
+    url(r'^components.json$', developer_views.jsonComponentsView, name='components-json'),
+    url(r'^moves.json$', developer_views.jsonMovesView, name='moves-json'),
 ]
