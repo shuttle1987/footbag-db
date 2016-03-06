a06d9c2cb3a0e1aeb2665f24d36c91de6dd0cc39
Janis <janis@jaggedverge.com>
2016-03-06 18:04:59 +1100

    Created json serializer for footbag components

--- apps/footbagmoves/developer_urls.py
+++ apps/footbagmoves/developer_urls.py
@@ -6,4 +6,5 @@ from apps.footbagmoves import developer_views
 urlpatterns = [
     url(r'^$', developer_views.index, name='developer-index'),
     url(r'^index$', developer_views.index, name='developer-index'),
+    url(r'^components_json$', developer_views.jsonComponentsView, name='developer-index'),
 ]
--- apps/footbagmoves/developer_views.py
+++ apps/footbagmoves/developer_views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from django.core import serializers
+
+from apps.footbagmoves.models import Component
+
+def jsonComponents():
+    """Serialize components into json"""
+    all_component_objects = list(Component.objects.all())
+    data = serializers.serialize('json', all_component_objects)
+    return data
+
 def index(request):
     template = loader.get_template('footbagmoves/developer.html')
     html = template.render({}, request)
     return HttpResponse(html)
+
+def jsonComponentsView(request):
+    """Respond to a request for the json serialization of the footbag components"""
+    components_json = jsonComponents()
+    return HttpResponse(components_json, content_type='application/json')
