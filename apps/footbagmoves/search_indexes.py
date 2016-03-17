cf54453061c943cefe838a5e5f5d493ec237470e
Janis <janis@jaggedverge.com>
2016-03-17 21:33:54 +1100

    Created rudimentary search index for footbag moves

--- apps/footbagmoves/search_indexes.py
+++ apps/footbagmoves/search_indexes.py
@@ -1,6 +1,6 @@
 """Indexing for the footbagmoves models for use with Haystack search"""
 from haystack import indexes
-from apps.footbagmoves.models import Component
+from apps.footbagmoves.models import Component, Move
 
 
 class ComponentIndex(indexes.SearchIndex, indexes.Indexable):
@@ -10,6 +10,18 @@ class ComponentIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Component
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
+
+class MoveIndex(indexes.SearchIndex, indexes.Indexable):
+    """Search indexing for the footbag Move objects"""
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Move
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
